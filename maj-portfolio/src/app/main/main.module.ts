1e967f5c37103c17f13153b1ad6b019bf090ec77
Mohammad Ali Jarjoumah <125255089+MohammadAliJa@users.noreply.github.com>
2025-01-20 12:00:20 +0200

    Added home page

--- maj-portfolio/src/app/main/main.module.ts
+++ maj-portfolio/src/app/main/main.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
+const routes: Routes = [
+  {
+    path: '', // This is the root path for this module
+    // component: YourFeatureComponent,
+    children: [
+      // { path: 'subroute1', component: Subroute1Component },
+      // { path: 'subroute2', component: Subroute2Component } 
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    HomeComponent,
+    CommonModule,
+    RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class MainModule { }
